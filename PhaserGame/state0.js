942ecd03af66aa2822d883cfccfb556067b80388
Phil <phillip5914619@gmail.com>
2017-02-18 13:25:30 -0800

    Adam moving around

--- PhaserGame/state0.js
+++ PhaserGame/state0.js
@@ -1,13 +1,35 @@
 var demo = {};
+var centerX = 1500 /2 ;
+var centerY = 1000 /2 ;
+var speed = 4;
+var adam;
+
 demo.state0 = function(){};
 demo.state0.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image('adam', './assets/sprites/adam.png');
+    },
     create: function(){
-        game.stage.backgroundColor = '#80ff80';
+        game.stage.backgroundColor = '#000000';
         console.log('state0'); 
         addChnageStateEventListeners();
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        adam = game.add.sprite(centerX, centerY , 'adam');
+        // adam.anchor.x = 0.5;
+        // adam.anchor.y = 0.5;
+        adam.anchor.setTo(0.5,0.5);
     },
-    update:function(){}
+    update:function(){
+        if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            adam.x += speed;
+        } else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            adam.x -= speed;
+        }else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+            adam.y -= speed;
+        }else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+            adam.y += speed;
+        }
+    }
 };
 
